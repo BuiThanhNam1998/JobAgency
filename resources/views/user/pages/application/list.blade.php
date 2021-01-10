cbde48c0bd20073bb7c40770994c30f2528d43cb
haianh1208 <n.haianh1208@gmail.com>
2021-01-11 01:48:01 +0700

    chinh sua menu

--- resources/views/user/pages/application/list.blade.php
+++ resources/views/user/pages/application/list.blade.php
@@ -58,31 +58,6 @@
                     @endforeach
                 </div>
 
-                <div class="col-md-4">
-                    <div class="contact-form">
-                        <div class="form-group">
-                            <h5>Blog Search</h5>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-8">
-                                <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2">
-                            </div>
-
-                            <div class="col-4">
-                                <button class="filled-button" type="button">Go</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <h5>Lorem ipsum dolor sit amet</h5>
-                    </div>
-
-                    <p><a href="blog-details.html">Lorem ipsum dolor sit amet, consectetur adipisicing.</a></p>
-                    <p><a href="blog-details.html">Et animi voluptatem, assumenda enim, consectetur quaerat!</a></p>
-                    <p><a href="blog-details.html">Ducimus magni eveniet sit doloremque molestiae alias mollitia vitae.</a></p>
-                </div>
             </div>
         </div>
     </div>
